3c76b80ac2891268601ca26f9899d09af695a2fd
webpage-admin <trumanenterprisetrust@gmail.com>
2025-10-20 15:56:14 +0100

    Update TermsAndCond.js

--- src/pages/TermsAndCond.js
+++ src/pages/TermsAndCond.js
@@ -17,7 +17,7 @@ const TermsAndCond = () => {
 
   return (
     <Container style={{ paddingBottom: "100px" }}>
-      <Header>Terms and Conditions</Header>
+      <Header>Terms and Conditions 2026</Header>
 
       <List>
         <li>
